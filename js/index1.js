1e9c3888be7a5f8a081e0837a8fdf69af225af85
Sourabh Kumar <79750363+sourabhkumar1999@users.noreply.github.com>
2021-02-28 16:45:08 +0530

    Update index1.js

--- js/index1.js
+++ js/index1.js
@@ -3,7 +3,7 @@ var randomNumber1 = Math.floor(Math.random() * 6) + 1; //1-6
 
 var randomDiceImage = "dice" + randomNumber1 + ".png"; //dice1.png - dice6.png
 
-var randomImageSource = "Images\" + randomDiceImage; //images/dice1.png - images/dice6.png
+var randomImageSource = "Images\" + randomDiceImage; //Images\dice1.png - Images\dice6.png
 
 var image1 = document.querySelectorAll("img")[0];
 
